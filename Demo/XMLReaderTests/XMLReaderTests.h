23b65ea720a245955ce02485c2e130042e5a42ad
David Perry <david.perry@WA12-AL12578.(none)>
2011-05-07 13:57:45 +0100

    Added demo project to show usage.

---
+++ Demo/XMLReaderTests/XMLReaderTests.h
@@ -0,0 +1,17 @@
+//
+//  XMLReaderTests.h
+//  XMLReaderTests
+//
+//  Created by David Perry on 07/05/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface XMLReaderTests : SenTestCase
+{
+@private
+    
+}
+
+@end
---
+++ Demo/XMLReaderTests/XMLReaderTests.m
@@ -0,0 +1,32 @@
+//
+//  XMLReaderTests.m
+//  XMLReaderTests
+//
+//  Created by David Perry on 07/05/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "XMLReaderTests.h"
+
+@implementation XMLReaderTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DemoTests");
+}
+
+@end
